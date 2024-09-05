dccd292c8779664e4d5c68a87b5ee19039abd019
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-09-05 18:00:22 +0200

    first in node

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    return JSON.parse((await fs.readFile(PATH_DB)).toString());
+  } catch (e) {
+    return e;
+  }
+};
